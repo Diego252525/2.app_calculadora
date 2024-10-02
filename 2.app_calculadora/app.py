31a0cb08283d55126eb188c0ecd1eb7a7507513f
Diego252525 <daprobtc123@gmail.com>
2024-10-02 12:32:14 -0500

    2.app_calculadora
    Versión 1.0

---
+++ 2.app_calculadora/app.py
@@ -0,0 +1,38 @@
+#importamos las clases y metodos
+from flask import Flask, render_template, redirect,request 
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET',"POST"])
+def aritmetica():
+    if request.method == "POST":
+        #valore que rcibo del form n1,n2 son pasados
+        num1 = float(request.form.get('n1'))
+        num2 = float(request.form.get('n2'))
+        #realizamos operacviones aritmeticas
+        suma = num1 + num2
+        resta = num1 - num2
+        multiplicacion = num1 * num2
+        division = num1/num2
+        return render_template('index.html', total_suma=suma,
+                                           total_resta=resta,
+                                           total_multiplicacion=multiplicacion,
+                                           total_division=division)
+    return render_template('index.html')
+
+@app.route('/divisas', methods=['GET', 'POST'])
+def convertir_divisa():
+    if request.method == 'POST':
+        pesos = float(request.form.get('pesos'))
+        dolar=4200
+        dolares = pesos*dolar
+        return render_template('divisas.html', total_dolar=dolares,
+                                             )
+    
+    return render_template('divisas.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
